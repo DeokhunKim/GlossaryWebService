eb5fa2bf2e841000b53a6169d0a062c72d51c4ed
DeokhunKim <86358502+DeokhunKim@users.noreply.github.com>
2022-05-11 16:35:34 +0900

    Add files via upload

---
+++ src/main/java/com/example/glossaryWebService/GlossaryWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.glossaryWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlossaryWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlossaryWebServiceApplication.class, args);
+	}
+
+}
